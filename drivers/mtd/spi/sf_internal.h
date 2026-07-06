gtxaspec/u-boot

commit 215f1d5794c6174ec0413eb6ff7f322ed284cc5b

mtd: spi-nor: Clear Winbond SR3 WPS bit on boot
Some Winbond SPI NORs have special SR3 register which is
used among other things to control whether non-standard
"Individual Block/Sector Write Protection" (WPS bit)
locking scheme is activated. This non-standard locking
scheme is not supported by either U-Boot or Linux SPI
NOR stack so make sure it is disabled, otherwise the
SPI NOR may appear locked for no obvious reason.

This SR3 WPS appears e.g. on W25Q16FW which has the same ID as
W25Q16DW, but the W25Q16DW does not implement the SR3 WPS bit.

Signed-off-by: Marek Vasut <marex@denx.de>

drivers/mtd/spi/spi-nor-core.c
@@ -595,6 +595,24 @@ static int read_cr(struct spi_nor *nor)
 }
 #endif
 
+/**
+ * read_sr3() - Read status register 3 unique to newer Winbond flashes
+ * @nor:	pointer to a 'struct spi_nor'
+ */
+static int read_sr3(struct spi_nor *nor)
+{
+	int ret;
+	u8 val;
+
+	ret = nor->read_reg(nor, SPINOR_OP_RDSR3, &val, 1);
+	if (ret < 0) {
+		dev_dbg(nor->dev, "error %d reading SR3\n", ret);
+		return ret;
+	}
+
+	return val;
+}
+
 /*
  * Write status register 1 byte
  * Returns negative if error occurred.
@@ -603,6 +621,17 @@ static int write_sr(struct spi_nor *nor, u8 val)
 {
 	nor->cmd_buf[0] = val;
 	return nor->write_reg(nor, SPINOR_OP_WRSR, nor->cmd_buf, 1);
+}
+
+/**
+ * write_sr3() - Write status register 3 unique to newer Winbond flashes
+ * @nor:	pointer to a 'struct spi_nor'
+ * @val:	value to be written into SR3
+ */
+static int write_sr3(struct spi_nor *nor, u8 val)
+{
+	nor->cmd_buf[0] = val;
+	return nor->write_reg(nor, SPINOR_OP_WRSR3, nor->cmd_buf, 1);
 }
 
 /*
@@ -4225,6 +4254,24 @@ static int spi_nor_init(struct spi_nor *nor)
 		write_enable(nor);
 		write_sr(nor, 0);
 		spi_nor_wait_till_ready(nor);
+
+		/*
+		 * Some Winbond SPI NORs have special SR3 register which is
+		 * used among other things to control whether non-standard
+		 * "Individual Block/Sector Write Protection" (WPS bit)
+		 * locking scheme is activated. This non-standard locking
+		 * scheme is not supported by either U-Boot or Linux SPI
+		 * NOR stack so make sure it is disabled, otherwise the
+		 * SPI NOR may appear locked for no obvious reason.
+		 */
+		if (JEDEC_MFR(nor->info) == SNOR_MFR_WINBOND) {
+			err = read_sr3(nor);
+			if (err > 0 && err & SR3_WPS) {
+				write_enable(nor);
+				write_sr3(nor, err & ~SR3_WPS);
+				write_disable(nor);
+			}
+		}
 	}
 
 	if (nor->quad_enable) {

include/linux/mtd/spi-nor.h
@@ -48,6 +48,8 @@
 #define SPINOR_OP_WRSR		0x01	/* Write status register 1 byte */
 #define SPINOR_OP_RDSR2		0x3f	/* Read status register 2 */
 #define SPINOR_OP_WRSR2		0x3e	/* Write status register 2 */
+#define SPINOR_OP_RDSR3		0x15	/* Read status register 3 */
+#define SPINOR_OP_WRSR3		0x11	/* Write status register 3 */
 #define SPINOR_OP_READ		0x03	/* Read data bytes (low frequency) */
 #define SPINOR_OP_READ_FAST	0x0b	/* Read data bytes (high frequency) */
 #define SPINOR_OP_READ_1_1_2	0x3b	/* Read data bytes (Dual Output SPI) */
@@ -185,6 +187,9 @@
  * in stacked/parallel configuration
  */
 #define SNOR_FLASH_CNT_MAX	2
+
+/* Status Register 3 bits. */
+#define SR3_WPS			BIT(2)
 
 /* For Cypress flash. */
 #define SPINOR_OP_RD_ANY_REG			0x65	/* Read any register */

commit 784c12d9056b1f986f8e0942d68a5d7a099ff7d0

mtd: spi-nor: Clear SR2 CMP / SR3 WPS on Winbond-compatible clones
The boot-time unlock (CONFIG_SPI_FLASH_UNLOCK_ALL) clears the SR3 WPS
and SR2 CMP protection bits only when the JEDEC manufacturer is Winbond
(0xef). Several W25Q-compatible clones use the same protection scheme
but report their own manufacturer IDs - Puya (0x85), Zbit (0x5e), Fudan
(0xa1) - so they were skipped and can come up locked for no obvious
reason.

Add a SPI_NOR_HAS_SR3_WPS flag and honour it alongside the Winbond
manufacturer check. Give the W25Q128-compatible clones py25q128ha,
zb25vq128 and fm25q128 the same SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB flags
as the genuine w25q128 - without them the parts never enter the unlock
path - plus SPI_NOR_HAS_SR3_WPS for the WPS/CMP clear.

A per-chip flag, rather than adding the clone manufacturer IDs to the
check, keeps the SR2/SR3 register writes confined to parts known to
implement them - a JEDEC first ID byte is not unique across vendors.

Fixes: 215f1d5794c6 ("mtd: spi-nor: Clear Winbond SR3 WPS bit on boot")

drivers/mtd/spi/sf_internal.h
@@ -69,6 +69,7 @@ struct flash_info {
 #define SPI_NOR_HAS_SST26LOCK	BIT(15)	/* Flash supports lock/unlock via BPR */
 #define SPI_NOR_OCTAL_READ	BIT(16)	/* Flash supports Octal Read */
 #define SPI_NOR_OCTAL_DTR_READ	BIT(17)	/* Flash supports Octal DTR Read */
+#define SPI_NOR_HAS_SR3_WPS	BIT(18)	/* Winbond-compatible SR2 CMP / SR3 WPS */
 };
 
 extern const struct flash_info spi_nor_ids[];

drivers/mtd/spi/spi-nor-core.c
@@ -4397,7 +4397,8 @@ static int spi_nor_init(struct spi_nor *nor)
 		 * NOR stack so make sure it is disabled, otherwise the
 		 * SPI NOR may appear locked for no obvious reason.
 		 */
-		if (JEDEC_MFR(nor->info) == SNOR_MFR_WINBOND) {
+		if (JEDEC_MFR(nor->info) == SNOR_MFR_WINBOND ||
+		    (nor->info->flags & SPI_NOR_HAS_SR3_WPS)) {
 			u8 cr;
 
 			err = read_sr3(nor);

drivers/mtd/spi/spi-nor-ids.c
@@ -138,7 +138,8 @@ const struct flash_info spi_nor_ids[] = {
 	{ INFO("fm25q64",    0xa14017, 0, 64 * 1024, 128,
 	       SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 	{ INFO("fm25q128",   0xa14018, 0, 64 * 1024, 256,
-	       SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+	       SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
+	       SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB | SPI_NOR_HAS_SR3_WPS) },
 	{ INFO("fm25w128",   0xa12818, 0, 64 * 1024, 256,
 	       SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 #endif
@@ -416,7 +417,8 @@ const struct flash_info spi_nor_ids[] = {
 	  SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 	{ INFO
 	 ("py25q128ha", 0x852018, 0, 64 * 1024, 256,
-	  SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+	  SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
+	  SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB | SPI_NOR_HAS_SR3_WPS) },
 	{ INFO
 	 ("py25f512hb", 0x85231a, 0, 64 * 1024, 1024,
 	  SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ | SPI_NOR_4B_OPCODES) },
@@ -759,7 +761,8 @@ const struct flash_info spi_nor_ids[] = {
 	{ INFO("zb25vq64",  0x5e4017, 0, 64 * 1024, 128,
 	       SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 	{ INFO("zb25vq128", 0x5e4018, 0, 64 * 1024, 256,
-	       SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+	       SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
+	       SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB | SPI_NOR_HAS_SR3_WPS) },
 #endif
 	{ },
 };